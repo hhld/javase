c01736983c15b2f5ef5cbdd205dded97993b271e
hhld <85204471+hhld@users.noreply.github.com>
2022-03-16 04:09:54 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author : lgh
+ * @version : 1.0
+ * @date : 15/3/2022 下午2:38
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
